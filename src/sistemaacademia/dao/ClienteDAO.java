e99a88a279034f07c048dd60fe4888b50122dd3a
Elian Batista <elianbatista2000@gmail.com>
2020-06-24 17:39:22 -0300

    Começo da criação do DAO de Clientes

--- src/sistemaacademia/dao/ClienteDAO.java
+++ src/sistemaacademia/dao/ClienteDAO.java
@@ -9,14 +9,15 @@ import java.sql.Connection;
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++ TIRAR
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import sistemaacademia.model.Cliente;
-// +++++++++++++++++++
-/**
- *
- * @author llcos_000
- */
+
 public class ClienteDAO {
     private Connection connection;
 
@@ -27,20 +28,39 @@ public class ClienteDAO {
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
-    //Tirar -------------------
-    private List<Cliente> listCliente = new ArrayList<Cliente>(){
-        {
-            add(new Cliente(0,"aaaa",new Date(2000,05,28),"111.111"));
-            add(new Cliente(0,"bbbb",new Date(2001,05,28),"222.222"));
-            add(new Cliente(0,"cccc",new Date(2001,05,28),"333.333"));
-        }
-    };
-    //------------------------
+    
     public List<Cliente> listar(){
-        return listCliente;
+        String sql = "SELECT * FROM clientes";
+        List<Cliente> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setId(resultado.getInt("id"));
+                cliente.setNome(resultado.getString("nome"));
+                cliente.setDataNascimento(resultado.getDate("nascimento"));
+                cliente.setCpf(resultado.getString("cpf"));
+                retorno.add(cliente);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
     }
-    public void inserir(Cliente cliente){
-        listCliente.add(cliente);
+    public boolean inserir(Cliente cliente){
+        String sql = "INSERT INTO clientes(nome, nascimento, cpf) VALUES(?,?,?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, cliente.getNome());
+            stmt.setDate(2, cliente.getDataNascimento());
+            stmt.setString(3, cliente.getCpf());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
     public void remover(Cliente cliente){
         listCliente.remove(cliente);  
