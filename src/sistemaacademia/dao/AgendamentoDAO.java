f0f958b59d08dfeb4b0db850edc0b7d77402f445
Elian Batista <elianbatista2000@gmail.com>
2020-06-27 00:05:41 -0300

    Término do TreinadorDAO

--- src/sistemaacademia/dao/AgendamentoDAO.java
+++ src/sistemaacademia/dao/AgendamentoDAO.java
@@ -6,14 +6,17 @@
 package sistemaacademia.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import sistemaacademia.model.Agendamento;
+import sistemaacademia.model.Cliente;
+import sistemaacademia.model.Treinador;
 
-/**
- *
- * @author llcos_000
- */
 public class AgendamentoDAO {
     private Connection connection;
 
@@ -24,17 +27,55 @@ public class AgendamentoDAO {
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
-    private List<Agendamento> listAgendamento = new ArrayList<>();
+    
     public List<Agendamento> listar(){
-        return listAgendamento;
-    }
-    public void inserir(Agendamento agendamento){
-        listAgendamento.add(agendamento);
+        //retorna lista com todos os agendamentos
+        String sql = "SELECT * FROM agendamentos";
+        List<Agendamento> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                Agendamento agendamento = new Agendamento();
+                agendamento.setId(resultado.getInt("id"));
+                agendamento.setDataInicio(resultado.getDate("data_inicio"));
+                agendamento.setHorario(resultado.getTime("horario"));
+                
+                //busca do treinador
+                Treinador treinadorAgendamento = new Treinador();
+                String sql2 = "SELECT * FROM treinadores WHERE id = ?";
+                PreparedStatement stmt2 = connection.prepareStatement(sql2);
+                stmt2.setInt(1, resultado.getInt("treinador_id"));
+                ResultSet resultado2 = stmt2.executeQuery();
+                if (resultado2.next()) {
+                    treinadorAgendamento.setId(resultado2.getInt("id"));
+                    treinadorAgendamento.setNome(resultado2.getString("nome"));
+                    treinadorAgendamento.setDataNascimento(resultado2.getDate("nascimento"));
+                    treinadorAgendamento.setCpf(resultado2.getString("cpf"));
+                    treinadorAgendamento.setCargaHoraria(resultado2.getInt("carga_horaria"));
+                    treinadorAgendamento.setValorPorHora(resultado2.getFloat("valor_por_hora"));
+                }
+                agendamento.setTreinador(treinadorAgendamento);
+                
+                //busca do cliente
+                Cliente clienteAgendamento = new Cliente();
+                String sql3 = "SELECT * FROM clientes WHERE id = ?";
+                PreparedStatement stmt3 = connection.prepareStatement(sql3);
+                stmt3.setInt(1, resultado.getInt("cliente_id"));
+                ResultSet resultado3 = stmt3.executeQuery();
+                if (resultado3.next()){
+                    clienteAgendamento.setId(resultado3.getInt("id"));
+                    clienteAgendamento.setNome(resultado3.getString("nome"));
+                    clienteAgendamento.setDataNascimento(resultado3.getDate("nascimento"));
+                    clienteAgendamento.setCpf(resultado3.getString("cpf"));
+                }
+                agendamento.setCliente(clienteAgendamento);
+                agendamento.setValor(resultado.getFloat("valor"));
+                retorno.add(agendamento);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AgendamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
     }
 }
-/*
-→ listar
-→ inserir agendamento
-→ Checar se está disponivel (no mes x e hora y, para o treinador z)
-→ quantidade de agendamentos de um treinador especifico
-*/
--- src/sistemaacademia/dao/TreinadorDAO.java
+++ src/sistemaacademia/dao/TreinadorDAO.java
@@ -7,15 +7,17 @@ package sistemaacademia.dao;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sistemaacademia.model.Agendamento;
 import sistemaacademia.model.Treinador;
 
-/**
- *
- * @author llcos_000
- */
 public class TreinadorDAO {
     private Connection connection;
 
@@ -26,36 +28,106 @@ public class TreinadorDAO {
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
-    // TIRAR ------------------------
-    private List<Treinador> listTreinador = new ArrayList<Treinador>(){
-        {
-            add(new Treinador(0,"Lucas",new Date(2000,3,20),"111.111",10,100));
-            add(new Treinador(0,"Elian",new Date(1000,3,20),"111.222",8,300));
-            add(new Treinador(0,"Douglas",new Date(5000,3,20),"333.222",5,400));
-        }
-    };
+    
     public List<Treinador> listar(){
-        return listTreinador;
+        //retorna lista com todos os treinadores
+        String sql = "SELECT * FROM treinadores";
+        List<Treinador> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                Treinador treinador = new Treinador();
+                treinador.setId(resultado.getInt("id"));
+                treinador.setNome(resultado.getString("nome"));
+                treinador.setDataNascimento(resultado.getDate("nascimento"));
+                treinador.setCpf(resultado.getString("cpf"));
+                treinador.setCargaHoraria(resultado.getInt("carga_horaria"));
+                treinador.setValorPorHora(resultado.getFloat("valor_por_hora"));
+                retorno.add(treinador);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
     }
+    
     public boolean estaDisponivel(Treinador treinador, Date data, Time hora){
         // Checa se o treinador está disponivel no mes e hora tal
-        return true;
+        String sql = "SELECT * FROM agendamentos WHERE treinador_id=? AND data_inicio = ? AND horario = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, treinador.getId());
+            stmt.setDate(2, data);
+            stmt.setTime(3, hora);
+            ResultSet resultado = stmt.executeQuery();
+            if (resultado.next()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
+    
+    public boolean checarCargaHoraria(Treinador treinador){
+        //checa se o treinador tem carga horária disponível ou não
+        String sql = "SELECT * FROM agendamentos WHERE treinador_id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, treinador.getId());
+            ResultSet resultado = stmt.executeQuery();
+            int cont = 0;
+            while (resultado.next()) {
+                cont++;
+            }
+            if(cont < treinador.getCargaHoraria()){
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
     public int getCargaHoraria(Treinador treinador){
-       return listTreinador.get(listTreinador.indexOf(treinador)).getCargaHoraria() - 3;
+       //retorna a carga horaria do treinador já subtraida da contidade de agendamento
+        String sql = "SELECT * FROM agendamentos WHERE treinador_id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, treinador.getId());
+            ResultSet resultado = stmt.executeQuery();
+            int cont = 0;
+            while (resultado.next()) {
+                cont++;
+            }
+            return treinador.getCargaHoraria() - cont;
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
     }
-    public float getvalorPorMes(Treinador treinador){
-        return listTreinador.get(listTreinador.indexOf(treinador)).getValorPorHora() * 20 + 10;
+    
+    public float getValorPorMes(Treinador treinador){
+        //retorna o valor cobrado por hora do treinador em questão
+        String sql = "SELECT * FROM agendamentos WHERE treinador_id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, treinador.getId());
+            ResultSet resultado = stmt.executeQuery();
+            int cont = 0;
+            while (resultado.next()) {
+                cont++;
+            }
+            return treinador.getValorPorHora() * 25 + cont * 10;
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
     }
-    public void diminuirCargaHoraria(Treinador treinador,int valor){
-        int cargaHoraria = listTreinador.get(listTreinador.indexOf(treinador)).getCargaHoraria();
-        listTreinador.get(listTreinador.indexOf(treinador)).setCargaHoraria(cargaHoraria - valor);
-    }
+    
 }
-/*
-
-→ listar 
-→ checar carga horaria (carga max - agendamentos
-→ valor por mes ( valor por hora * 25 + quantidade de agendamentos * 10)
-
-*/
